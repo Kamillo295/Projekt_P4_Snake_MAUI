6baf327fc92c8320f7973e37097d26f7538907cd
Kamillo295 <kammil2005@gmail.com>
2026-05-21 15:00:39 +0200

    models

---
+++ SnakeMaui/Models/Position.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeMaui.Models
+{
+    internal class Position
+    {
+    }
+}
---
+++ SnakeMaui/Services/Implementations/GameService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeMaui.Services.Implementations
+{
+    internal class GameService
+    {
+    }
+}
